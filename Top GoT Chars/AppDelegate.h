dcd9510e03f9ad05a044052f33c74273913a6b15
Kacper Augustyniak <Yosemite@macbook-pro-kacper.local>
2015-10-29 22:17:30 +0100

    Initial Commit

---
+++ Top GoT Chars/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Top GoT Chars
+//
+//  Created by Kacper Augustyniak on 29.10.2015.
+//  Copyright © 2015 Kacper Augustyniak. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Top GoT Chars/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Top GoT Chars
+//
+//  Created by Kacper Augustyniak on 29.10.2015.
+//  Copyright © 2015 Kacper Augustyniak. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
